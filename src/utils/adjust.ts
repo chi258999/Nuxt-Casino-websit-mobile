96eb2839539f5268b3f74ef816be37ef691ee65a
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-03-18 18:48:56 +0100

    feat:event tracking

--- src/utils/adjust.ts
+++ src/utils/adjust.ts
@@ -1,6 +1,14 @@
 
 import Adjust from "@adjustcom/adjust-web-sdk";
 
-export function adjustTrackEvent(param: any): void {
-    Adjust.trackEvent(param);
+function isRunningAsMobileWebApp(): boolean {
+  return window.matchMedia('(display-mode: standalone)').matches || (navigator as any).standalone;
+}
+
+export function adjustTrackEvent(key: any, value = null as any): void {
+  if (isRunningAsMobileWebApp()) {
+    (window as any)["AndroidWebView"].firebaseEvent(key, value)
+  } else  {
+    Adjust.trackEvent(key);
+  }
 }
