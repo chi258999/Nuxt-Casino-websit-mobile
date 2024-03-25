890c038f597d22b9246294907b62df26e4dcb48b
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-03-26 00:54:40 +0800

    fix:注册登录界面，不能上下拉动小屏修复

--- src/components/auth/components/mobile/sign_in/index.vue
+++ src/components/auth/components/mobile/sign_in/index.vue
@@ -740,7 +740,7 @@ export default Login;
   background: $color_1;
   overflow-y: auto;
   height: calc(100vh - 50px);
-  padding-bottom: 80px;
+  padding-bottom: 120px;
 
   .v-field--variant-solo {
     background: transparent !important;
