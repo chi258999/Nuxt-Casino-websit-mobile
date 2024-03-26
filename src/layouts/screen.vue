b773106e45558086f2ba8e189560681fe49cd918
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-03-27 00:28:18 +0800

    fix:h5横屏时出现遮罩层

---
+++ src/layouts/screen.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="screen"></div>
+</template>
+
+<style lang="scss">
+.screen {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000000009;
+  background: url("@/assets/common/screen.png");
+  background-size: cover;
+}
+</style>
