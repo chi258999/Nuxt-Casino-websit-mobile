44df557f638b3fd9acbba71ece0c166c290901fe
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-03-27 00:46:42 +0800

    fix: 移动端屏幕旋转

--- src/layouts/screen.vue
+++ src/layouts/screen.vue
@@ -1,7 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const downLoadApp = () => {
+  console.log("downLoadApp");
+};
+</script>
 
 <template>
-  <div class="screen"></div>
+  <div class="screen">
+    <div class="screen-tips">
+      <img class="screen-img" src="@/assets/common/screen-phone.jpg" />
+      <span class="tips-text">Rotate phone screen</span>
+    </div>
+    <div class="down-btn" @click="downLoadApp"></div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -12,7 +22,33 @@
   left: 0;
   right: 0;
   z-index: 1000000009;
-  background: url("@/assets/common/screen.png");
-  background-size: cover;
+//   background: url("@/assets/common/screen.jpg");
+//   background-size: cover;
+  background: #000;
+  padding: 50px;
+  .screen-tips {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .screen-img {
+      width: 48px;
+      height: 48px;
+    }
+    .tips-text {
+      color: #7782aa;
+      font-size: 12px;
+      margin-top: 16px;
+    }
+  }
+  .down-btn {
+    width: 310px;
+    height: 76px;
+    background: url("@/assets/common/downLoad.jpg");
+    background-size: cover;
+    position: absolute;
+    left: 50%;
+    bottom: 50px;
+    transform: translate(-50%,0);
+  }
 }
 </style>
