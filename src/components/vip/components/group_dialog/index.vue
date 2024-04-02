c9184dc2481e21e2470e1263a8a6e45449cd2671
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-04-02 21:31:51 +0800

    fix：新增group弹窗

---
+++ src/components/vip/components/group_dialog/index.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+  },
+});
+
+const emit = defineEmits(["update:modelValue", "dialogHandle"]);
+
+const modelValueNew = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(val) {
+    emit("update:modelValue", val);
+  },
+});
+
+const closeGroupDialog = () => {
+  emit("update:modelValue", false);
+};
+</script>
+
+<template>
+  <v-dialog v-model="modelValueNew" @click:outside="closeGroupDialog">
+    <div>
+      
+    </div>
+  </v-dialog>
+</template>
+
+<style lang="scss"></style>
