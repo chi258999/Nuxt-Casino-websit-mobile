ed55a25accd4b1428abbf2b2e560d5f7c44d810e
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-04-05 04:29:23 +0800

    facebook中的token字符修改

--- src/plugins/third-party-login.ts
+++ src/plugins/third-party-login.ts
@@ -57,7 +57,7 @@ const loginWithSocialMedia = async (value: string, type: string): Promise<any> =
  * 判断登录和注册
  * @param type 
  */
-const loginOrRegister = async (token: string, value: string, type: string) => {
+const loginOrRegister = (token: string, value: string, type: string) => {
   console.log(value, type, 'loginOrRegister');
 
     let val = 1;
@@ -74,10 +74,10 @@ const loginOrRegister = async (token: string, value: string, type: string) => {
     
     if (type === "login") {
         // 登录
-        await dispatchQuickLogin(params);
+        dispatchQuickLogin(params);
     } else {
         // 注册
-        await dispatchQuickRegister(params);
+        dispatchQuickRegister(params);
     }
 }
 
@@ -125,7 +125,7 @@ const loginWithFacebook = (value: string, type: string): Promise<any> => {
         if (response.authResponse) {
           globalWindow.FB.login((res: any) => {
             console.log(res, 'FB.login');
-            loginOrRegister(res.authResponse.access_token, value, type);
+            loginOrRegister(res.authResponse.accessToken, value, type);
           });
         } else {
           reject(response.status);
