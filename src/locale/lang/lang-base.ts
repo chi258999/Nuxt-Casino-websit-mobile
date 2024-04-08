10cbcd21ff37198af75d1545035393987e19509b
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-04-08 22:16:30 +0800

    fix: 邀请文本更改

--- src/locale/lang/lang-base.ts
+++ src/locale/lang/lang-base.ts
@@ -1502,8 +1502,14 @@ export type langType = {
     description: {
       text_1: string
       text_2: string
-      term_text: string
       text_3: string
+      term_text_1:string
+      term_text_2:string
+      term_text_3:string
+      text_4: string
+      term_text_4: string
+      text_5:string
+      text_6: string
       back_btn_text: string
     }
   }
