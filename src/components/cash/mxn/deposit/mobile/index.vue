cd7b3e902d73e5fbeac561ab24ca7909922e4c91
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-04-23 01:34:54 +0800

    存款时弹出：0-1000000的存款金额提示

--- src/components/cash/mxn/deposit/mobile/index.vue
+++ src/components/cash/mxn/deposit/mobile/index.vue
@@ -359,6 +359,8 @@ const validateAmount = (): boolean => {
 }
 
 const handleAmountInputFocus = (): void => {
+  console.log('handleAmountInputFocus');
+  
   if (validateAmount()) {
     isShowAmountValidation.value = false;
   } else {
@@ -1001,6 +1003,8 @@ onMounted(async () => {
         </v-col>
       </v-row>
       <v-row class="mt-3 mx-3 relative">
+        <!-- :onfocus="handleAmountInputBlur"
+          :onblur="handleAmountInputFocus" -->
         <v-text-field
           :label="`${t('deposit_dialog.amount')}(${platformCurrency})`"
           class="form-textfield dark-textfield m-deposit-amount-text"
@@ -1009,8 +1013,6 @@ onMounted(async () => {
           color="#7782AA"
           style="border-radius: 8px"
           v-model="depositAmount"
-          :onfocus="handleAmountInputBlur"
-          :onblur="handleAmountInputFocus"
           @input="handleAmountInputChange"
         />
         <ValidationBox
