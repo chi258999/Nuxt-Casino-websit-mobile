fbcbcc6cf629c5900d34ad418349396f5ef9ca6c
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-04-24 18:35:37 +0800

    代理页面 profit 查询加底部滚动样式 + 样式优化

--- src/components/agent/profit/index.vue
+++ src/components/agent/profit/index.vue
@@ -87,148 +87,150 @@ onMounted(async () => {
 </script>
 <template>
   <div class="m-agent-profit-summary-card-box">
-    <div class="m-agent-profit-summary-card mx-2" ref="profitSummaryCard" @scroll="containerScroll">
-      <div class="ma-4 text-800-14 white">{{ t("affiliate.statistics.summary_text") }}</div>
-      <v-row class="mx-1 my-0 px-6 m-agent-profit-summary-level">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          {{ t("affiliate.statistics.level_text") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          {{ t("affiliate.statistics.grade_text_1") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          {{ t("affiliate.statistics.grade_text_2") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          {{ t("affiliate.statistics.grade_text_3") }}
-        </v-col>
-      </v-row>
+    <div class="m-agent-profit-summary-card_c">
+      <div class="text-800-14 white m-agent-profit-summary-card_c_title">{{ t("affiliate.statistics.summary_text") }}</div>
+      <div class="m-agent-profit-summary-card" ref="profitSummaryCard" @scroll="containerScroll">
+        <v-row class="my-0 px-6 m-agent-profit-summary-level">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            {{ t("affiliate.statistics.level_text") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            {{ t("affiliate.statistics.grade_text_1") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            {{ t("affiliate.statistics.grade_text_2") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            {{ t("affiliate.statistics.grade_text_3") }}
+          </v-col>
+        </v-row>
 
-      <!-- 行Registered Users -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-user-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_4") }}
-          <br /> 3
-          {{ t("affiliate.statistics.text_5") }} -->
-          {{ t("affiliate.statistics.text_12") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.register_user[0] }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.register_user[1] }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.register_user[2] }}
-        </v-col>
-      </v-row>
+        <!-- 行Registered Users -->
+        <v-row class="my-1 px-6 m-agent-profit-user-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_4") }}
+            <br /> 3
+            {{ t("affiliate.statistics.text_5") }} -->
+            {{ t("affiliate.statistics.text_12") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.register_user[0] }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.register_user[1] }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.register_user[2] }}
+          </v-col>
+        </v-row>
 
-      <!-- 行 Deposit Users -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-user-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_6") }}
-          <br /> 4
-          {{ t("affiliate.statistics.text_5") }} -->
-          {{ t("affiliate.statistics.text_13") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.deposit_user[0] }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.deposit_user[1] }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ selectedItem.deposit_user[2] }}
-        </v-col>
-      </v-row>
+        <!-- 行 Deposit Users -->
+        <v-row class="my-1 px-6 m-agent-profit-user-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_6") }}
+            <br /> 4
+            {{ t("affiliate.statistics.text_5") }} -->
+            {{ t("affiliate.statistics.text_13") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.deposit_user[0] }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.deposit_user[1] }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ selectedItem.deposit_user[2] }}
+          </v-col>
+        </v-row>
 
-      <!-- 行 Commission Amount -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-commission-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_8") }}
-          <br /> 5
-          {{ t("affiliate.statistics.text_7") }} -->
-          {{ t("affiliate.statistics.text_14") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 yellow">
-          {{ platformCurrency }} {{ Number(selectedItem.deposit_bonus).toFixed(2) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
-      </v-row>
+        <!-- 行 Commission Amount -->
+        <v-row class="my-1 px-6 m-agent-profit-commission-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_8") }}
+            <br /> 5
+            {{ t("affiliate.statistics.text_7") }} -->
+            {{ t("affiliate.statistics.text_14") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 yellow">
+            {{ platformCurrency }} {{ Number(selectedItem.deposit_bonus).toFixed(2) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
+        </v-row>
 
-      <!-- 行 Deposit Amount -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-user-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_6") }}
-          <br /> 6
-          {{ t("affiliate.statistics.text_7") }} -->
-          {{ t("affiliate.statistics.text_15") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[0]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[1]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[2]) }}
-        </v-col>
-      </v-row>
+        <!-- 行 Deposit Amount -->
+        <v-row class="my-1 px-6 m-agent-profit-user-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_6") }}
+            <br /> 6
+            {{ t("affiliate.statistics.text_7") }} -->
+            {{ t("affiliate.statistics.text_15") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[0]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[1]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.deposit_amount[2]) }}
+          </v-col>
+        </v-row>
 
-      <!-- 行 Bet Amount -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-user-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_9") }}
-          <br /> 7
-          {{ t("affiliate.statistics.text_7") }} -->
-          {{ t("affiliate.statistics.text_16") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[0]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[1]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[2]) }}
-        </v-col>
-      </v-row>
+        <!-- 行 Bet Amount -->
+        <v-row class="my-1 px-6 m-agent-profit-user-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_9") }}
+            <br /> 7
+            {{ t("affiliate.statistics.text_7") }} -->
+            {{ t("affiliate.statistics.text_16") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[0]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[1]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_amount[2]) }}
+          </v-col>
+        </v-row>
 
-      <!-- 行 Commission Amount -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-commission-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_8") }}
-          <br /> 8
-          {{ t("affiliate.statistics.text_7") }} -->
-          {{ t("affiliate.statistics.text_17") }}
+        <!-- 行 Commission Amount -->
+        <v-row class="my-1 px-6 m-agent-profit-commission-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_8") }}
+            <br /> 8
+            {{ t("affiliate.statistics.text_7") }} -->
+            {{ t("affiliate.statistics.text_17") }}
 
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 yellow">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[0]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 yellow">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[1]) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 yellow">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[2]) }}
-        </v-col>
-      </v-row>
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 yellow">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[0]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 yellow">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[1]) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 yellow">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.bet_bonus[2]) }}
+          </v-col>
+        </v-row>
 
-      <!-- 行 Achievement Reward -->
-      <v-row class="mx-1 my-1 px-6 m-agent-profit-user-card">
-        <v-col cols="3" class="pa-0 text-400-12 gray">
-          <!-- {{ t("affiliate.statistics.text_10") }}
-          <br /> 9
-          {{ t("affiliate.statistics.text_11") }} -->
-          {{ t("affiliate.statistics.text_18") }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray">
-          {{ platformCurrency }} {{ toFormatNum(selectedItem.achievement_award) }}
-        </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
-        <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
-      </v-row>
+        <!-- 行 Achievement Reward -->
+        <v-row class="my-1 px-6 m-agent-profit-user-card">
+          <v-col cols="3" class="pa-0 text-400-12 gray">
+            <!-- {{ t("affiliate.statistics.text_10") }}
+            <br /> 9
+            {{ t("affiliate.statistics.text_11") }} -->
+            {{ t("affiliate.statistics.text_18") }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray">
+            {{ platformCurrency }} {{ toFormatNum(selectedItem.achievement_award) }}
+          </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
+          <v-col cols="3" class="pa-0 text-700-12 gray"> -- </v-col>
+        </v-row>
+      </div>
     </div>
     <div class="arrow" :class="['arrow-absolute']" v-if="containerShowMoreArrow">
       <img src="@/assets/public/svg/arrow-right.svg" alt="">
@@ -276,6 +278,7 @@ onMounted(async () => {
   height: fit-content;
   width: 100%;
   position: relative;
+  padding: 0 16px;
 
   .arrow {
     display: flex;
@@ -287,6 +290,7 @@ onMounted(async () => {
     height: 39px;
     border-radius: 4px 0px 0px 4px;
     background: #000;
+    opacity: 0.5;
 
     img {
       width: 14px;
@@ -295,27 +299,40 @@ onMounted(async () => {
   }
   .arrow-absolute {
     position: absolute;
-    right: 6px;
+    right: 16px;
     top: 50%;
     transform: translateY(-50%);
   }
 }
-.m-agent-profit-summary-card {
-  border-radius: 8px;
+
+.m-agent-profit-summary-card_c {
+  padding-bottom: 8px;
   background: $agent_card_notmet_bg;
   box-shadow: 0px 4px 6px 1px rgba(0, 0, 0, 0.3);
+  border-radius: 8px;
+}
+.m-agent-profit-summary-card_c_title {
+  padding: 16px;
+}
+.m-agent-profit-summary-card {
   overflow-x: auto;
   position: relative;
+  padding-bottom: 4px;
+  margin-left: 4px;
+  margin-right: 4px;
 }
 
 .m-agent-profit-summary-card::-webkit-scrollbar-thumb {
-  background: $agent_card_bg;
+  background: #303543;
   border-radius: 20px;
 }
 
 .m-agent-profit-summary-card::-webkit-scrollbar {
   width: 3px;
-  height: 6px;
+  height: 8px;
+  // scroll-padding: 20px;
+}
+.m-agent-profit-summary-card::-webkit-scrollbar-track {
   scroll-padding: 20px;
 }
 
