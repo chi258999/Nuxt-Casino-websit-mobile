16c2ac5c908618f68fd48e7409af930a2be2fb0d
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-05-07 22:10:14 +0800

    首次加两加入 loding ,一直到首页内容加载完成后消失，动画为拉霸机的表现形式

---
+++ src/components/first_screen_skeleton/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+import { storeToRefs } from "pinia";
+import { useI18n } from "vue-i18n";
+
+</script>
+
+<template>
+  <div class="first">
+    <img src="@/assets/first-animation.gif" alt="">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.first {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100VH;
+  background: #15161c;
+  z-index: 111111111111111111111111111111;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100px;
+  }
+}
+
+</style>
