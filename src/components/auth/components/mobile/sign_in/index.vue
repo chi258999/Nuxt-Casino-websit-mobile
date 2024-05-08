e432018391dc3d1fdc6ec4a8218de4aa16caa579
juliusespinosa991 <juliusespinosa991@gmail.com>
2024-05-08 22:17:49 +0800

    弹窗弹出顺序:VIP连续登入奖金弹窗没有自动弹出

--- src/components/auth/components/mobile/sign_in/index.vue
+++ src/components/auth/components/mobile/sign_in/index.vue
@@ -65,7 +65,7 @@ const Login = defineComponent({
     const { setToken } = authStore()
     const { dispatchSocketConnect } = socketStore()
     const { dispatchUserInvite } = inviteStore()
-    const { dispatchVipInfo } = vipStore()
+    const { dispatchVipInfo, dispatchVipSignIn } = vipStore()
     const { dispatchVipLevels } = vipStore()
     const { dispatchVipLevelAward } = vipStore()
     const { width } = useDisplay()
@@ -170,6 +170,7 @@ const Login = defineComponent({
         await dispatchVipInfo()
         await dispatchVipLevels()
         await dispatchVipLevelAward()
+        await dispatchVipSignIn();
 
         // 获取服务器时间戳
         dispatchTimeunix()
@@ -256,6 +257,11 @@ const Login = defineComponent({
         localStorage.setItem(userInfo.value.name, '1')
       }
 
+      // 打开VIP活动签到  is_signin = 2就是领取了
+      if (userInfo.value.id && vipSignIn.value.is_signin != 2) {
+        setLoginBonusDialogVisible(true)
+      }
+
       setTimeout(async () => {
         const queryParams = getQueryParams()
         // 如果用户是app登录，那就领取奖励
@@ -276,10 +282,7 @@ const Login = defineComponent({
               rtl: false,
             })
             await userDownloadAppAcquisition()
-            // 打开VIP活动签到
-            if (userInfo.value.id && vipSignIn.value.is_signin != 2) {
-              setLoginBonusDialogVisible(true)
-            }
+
           } catch (error) {}
         }
       }, 500)
